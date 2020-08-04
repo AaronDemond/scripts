9b0b1147c61f57c001bdf8819b4ded53d36eb460
AaronDemond <aarondemond@dal.ca>
2020-08-04 12:58:09 -0700

    Scripts from the easternweb project

---
+++ gatherer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+start website/website/trade-gatherer.py
+start website/easternweburl.url
+start website/website/price-gatherer.py
+echo "Hello"
---
+++ start-server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python website/manage.py runserver
+
